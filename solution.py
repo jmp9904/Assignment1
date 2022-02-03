6b4a881dc1f16f38e96b0da5210ba2039c229c84
jmp9904 <95732066+jmp9904@users.noreply.github.com>
2022-02-03 11:00:34 -0600

    Add files via upload

--- solution.py
+++ solution.py
@@ -23,6 +23,8 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
+    else:
+        answer = "mtls"
     return(answer)
 # Complete all the questions.
 
